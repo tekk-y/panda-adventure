a26252eb07fea0a71419e6976de292f41f35108e
tekky <84053058+tekk-y@users.noreply.github.com>
2023-06-06 21:16:51 +0200

    add zap logger

--- main.go
+++ main.go
@@ -1,33 +1,27 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"net/http"
+
+	"go.uber.org/zap"
 )
 
 func main() {
-        log.Print("starting server...")
-        http.HandleFunc("/", handler)
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
 
-        port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-                log.Printf("defaulting to port %s", port)
-        }
+	http.HandleFunc("/", handler)
 
-        log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-                log.Fatal(err)
-        }
+	port := "8080"
+	logger.Info("starting server", zap.String("port", port))
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		logger.Error("error", zap.Error(err))
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        name := os.Getenv("NAME")
-        if name == "" {
-                name = "World"
-        }
-        fmt.Fprintf(w, "Hello %s!\n", name)
+	header := w.Header()
+	header.Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"hello": "world"}`)
 }
-
